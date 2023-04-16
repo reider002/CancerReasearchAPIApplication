f73fad95866778921d4431fcb8e5f6309d75a336
Reider737 <Reider737@gmail.com>
2023-04-16 20:52:14 +0000

    pre for biopython

---
+++ src/views/Ensemble.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <button @click="runBiopython()">Run Biopython</button>
+      <div v-if="biopythonResult">
+        {{ biopythonResult }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        biopythonResult: null
+      };
+    },
+    methods: {
+      runBiopython() {
+          //TODO: replace api call with correct endpoint from Flask Biopython connection
+        axios.get('/api/biopython').then(response => {
+          this.biopythonResult = response.data.result;
+        }).catch(error => {
+          console.error(error);
+        });
+      }
+    }
+  };
+  </script>
+  
