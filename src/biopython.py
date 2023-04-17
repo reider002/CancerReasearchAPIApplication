782a29f3739513c57de21ec835f3a763afd12e1f
Reider737 <Reider737@gmail.com>
2023-04-17 22:27:08 +0000

    working biopython script

--- src/biopython.py
+++ src/biopython.py
@@ -1,14 +1,14 @@
-from biopython import SeqIO
-from biopython import SeqRecord, SeqFeature, FeatureLocation
+from Bio import SeqIO
+#from Bio import SeqRecord
+#from Bio import SeqFeature
+#from Bio import FeatureLocation
 
 #Set up the Ensembl URL
 #TODO: impliment adjustable URL parameters - http://rest.ensembl.org/overlap/region/<species>/<region>?feature=exon
-ensembl_url = "http://nov2020.archive.ensembl.org/Homo_sapiens/Location/View?" \
-              "db=core;g=ENSG00000134982;r=5:112707498-112846239"
+ensembl_url = "http://nov2020.archive.ensembl.org/Homo_sapiens/Location/View?db=core;g=ENSG00000134982;r=5:112707498-112846239"
 
 #Retrieve the sequence data from the Ensembl URL
 sequence_record = SeqIO.read(ensembl_url, "fasta")
-
 #Extract the exon features from the sequence record
 exon_features = [feature for feature in sequence_record.features if feature.type == "exon"]
 
