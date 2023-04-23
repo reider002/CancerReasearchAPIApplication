44bd6e863af28ac628802cf91a1804c86fb13f5b
Reider737 <Reider737@gmail.com>
2023-04-23 18:48:35 +0000

    repo cleanup, setup for Flask client to use Biopython

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,6 +5,12 @@
     <ul>
       <li v-for="result in results" :key="result.id">{{ result.name }}</li>
     </ul>
+    <div v-if="exonData">
+      <h2>Exons for gene {{ selectedGene }}:</h2>
+      <ul>
+        <li v-for="exon in exonData" :key="exon.id">{{ exon.id }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -16,18 +22,39 @@ export default {
     return {
       searchText: '',
       results: [],
+      selectedGene: '',
+      exonData: null,
     };
   },
   methods: {
     search() {
       //TODO: change placeholder URL, replace with parsed URL containing users search criteria
-      axios.get('https://api.gdc.cancer.gov/files/38f3a6d7-7852-5775-858a-2fcaabac54d6?pretty=true', {
+      axios.get('', {
         params: {
           q: this.searchText,
         },
       })
         .then(response => {
           this.results = response.data.results;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getExonData(geneId, start, end) {
+      // Define the Ensembl REST API endpoint and parameters
+      const ensemblApiUrl = "https://rest.ensembl.org";
+      const exonDataEndpoint = `/overlap/region/human/${start}-${end}?feature=exon;feature=gene;content-type=application/json;gene=${geneId}`;
+
+      // Send the GET request to the Ensembl REST API
+      axios.get(ensemblApiUrl + exonDataEndpoint)
+        .then(response => {
+          // Parse the JSON response and extract the exon data
+          const exonData = response.data;
+          const exons = exonData.filter(exon => exon.feature_type === "exon");
+          // Set the exon data and selected gene in the component's data
+          this.exonData = exons;
+          this.selectedGene = geneId;
         })
         .catch(error => {
           console.log(error);
